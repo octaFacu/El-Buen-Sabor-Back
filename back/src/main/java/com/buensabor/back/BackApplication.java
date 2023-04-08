3c4e566edc5e7a8ba0a22fcb586a9a9a1196b246
pazfernandez <mariapazfernandezravaglia@gmail.com>
2023-04-08 12:19:47 -0300

    Añadida aplicación base Spring

---
+++ back/src/main/java/com/buensabor/back/BackApplication.java
@@ -0,0 +1,20 @@
+package com.buensabor.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, World!";
+	}
+}
