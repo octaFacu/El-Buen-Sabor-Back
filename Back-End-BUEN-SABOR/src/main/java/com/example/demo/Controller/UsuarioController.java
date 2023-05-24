23bf15c1710fa5f27371ccc9c758e8a580169ffe
Hugo Sanchez <hugo127sanchez@gmail.com>
2023-05-24 20:41:56 -0300

    Agrege seguridad basica hay que mejorarlo

--- Back-End-BUEN-SABOR/src/main/java/com/example/demo/Controller/UsuarioController.java
+++ Back-End-BUEN-SABOR/src/main/java/com/example/demo/Controller/UsuarioController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping(path = "/usuario")
 public class UsuarioController extends GenericControllerImpl<Usuario, ImpUsuarioService> {
 }
