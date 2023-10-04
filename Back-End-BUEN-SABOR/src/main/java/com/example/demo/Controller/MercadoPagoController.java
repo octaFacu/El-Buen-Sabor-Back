cfcd8c8299bd2a2b0c133267d9584c9b38e2a83d
Octavio <octavio.curadelli@alumnos.frm.utn.edu.ar>
2023-10-03 23:41:35 -0300

    Verificacion de pago por mercado pago en controlador de mercado pago

--- Back-End-BUEN-SABOR/src/main/java/com/example/demo/Controller/MercadoPagoController.java
+++ Back-End-BUEN-SABOR/src/main/java/com/example/demo/Controller/MercadoPagoController.java
@@ -2,21 +2,24 @@ package com.example.demo.Controller;
 
 // SDK de Mercado Pago
 import com.example.demo.Entidades.Producto;
+import com.example.demo.Entidades.Wrapper.NotificacionMP;
 import com.example.demo.Entidades.Wrapper.ProductoParaPedidoMP;
 import com.example.demo.Entidades.Wrapper.RequestDataMP;
 import com.example.demo.Entidades.Wrapper.UserAuth0MP;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import com.mercadopago.MercadoPagoConfig;
+import com.mercadopago.client.merchantorder.MerchantOrderClient;
 import com.mercadopago.client.payment.PaymentClient;
 import com.mercadopago.client.preference.*;
-import com.mercadopago.resources.common.Identification;
+import com.mercadopago.resources.merchantorder.MerchantOrder;
+import com.mercadopago.resources.merchantorder.MerchantOrderPayment;
 import com.mercadopago.resources.payment.Payment;
-import com.mercadopago.resources.payment.PaymentPayer;
 import com.mercadopago.resources.preference.Preference;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -102,7 +105,7 @@ public class MercadoPagoController {
                     .backUrls(backUrls)
                     .autoReturn("approved")
                     .marketplace("El Buen Sabor")
-                    .notificationUrl("https://8437-2803-9800-9846-68b-a5d4-18c4-37a6-21bd.ngrok-free.app/mp/notificacionPago")
+                    .notificationUrl("https://5085-2803-9800-9846-68b-659a-e7cc-5977-3ba5.ngrok-free.app/mp/notificacionPago")
                     .build();
 
             PreferenceClient client = new PreferenceClient();
@@ -124,6 +127,7 @@ public class MercadoPagoController {
 
     }
 
+    //HACER PERSISITENCIA DEL PEDIDO DESDE CONTROLADOR MERADO PAGO, PASANDO EL ID DEL PEDIDO POR LE PATH DE notificationUrl
 
     @PostMapping("/notificacionPago")
     public ResponseEntity<?> notificacionDelPago(@RequestBody String notificationData) {
@@ -131,20 +135,88 @@ public class MercadoPagoController {
         System.out.println("Notificaciooooooooooon");
         System.out.println(notificationData);
 
-
         try{
             // Agrega credenciales
             MercadoPagoConfig.setAccessToken(accessToken);
 
+            NotificacionMP notification = null;
+            ObjectMapper objectMapper = new ObjectMapper();
+            String topic = null;
+            Long idRequest = null;
+
+            try {
+                // Convierte la cadena JSON en un objeto Java
+                notification = objectMapper.readValue(notificationData, NotificacionMP.class);
+                topic = notification.getTopic();
+
+                String[] partes = notification.getResource().split("/");
+                idRequest = Long.parseLong(partes[partes.length - 1]);
+
+                //System.out.println("Resource: " + notification.getResource());
+                //System.out.println("Topic: " + notification.getTopic());
+            } catch (UnrecognizedPropertyException e) {
+                System.out.println("Ignoro el objeto que no tiene el formato deseado");
+            } catch (JsonProcessingException e) {
+                System.out.println("Ignoro el objeto que no tiene el formato deseado");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            MerchantOrderClient moc = null;
+            MerchantOrder mo = null;
+
+            if(topic != null){
+                switch (topic){
+                    case "merchant_order":
+                        moc = new MerchantOrderClient();
+
+                        mo = moc.get(idRequest);
+
+                        System.out.println("merchant order");
+                        System.out.println(mo.getOrderStatus());
+                        break;
+                    case "payment":
+                        PaymentClient pc = new PaymentClient();
+                        moc = new MerchantOrderClient();
+
+                        Payment p = pc.get(idRequest);
+                        System.out.println(p.getStatus());
+                        mo = moc.get(p.getOrder().getId());
+
+                        System.out.println("payment");
+                        System.out.println(mo.getOrderStatus());
+                        break;
+                    default:
+
+                        break;
+                }
+
+                BigDecimal paidAmount = new BigDecimal(0);
+
+                //Traigo la lista de los pagos
+                List<MerchantOrderPayment> mop = mo.getPayments();
+                //Por cada pago lo agrego al pago total
+                for (MerchantOrderPayment pago : mop) {
+                    if(pago.getStatus().equals("approved")){
+                        System.out.println("Es aprobado");
+                        paidAmount = paidAmount.add(pago.getTransactionAmount());
+                    }
+                }
+
+                //Si se pago el total del monto a pagar
+                if(paidAmount.compareTo(mo.getTotalAmount()) == 0 || paidAmount.compareTo(mo.getTotalAmount()) == 1){
+                    System.out.println("Total a pagar " + mo.getTotalAmount().toString());
+                    System.out.println("Total PAGADO " + paidAmount.toString());
+
+                }else{
+                    System.out.println("EL PAGO NO SE COMPLETÓ");
+                }
+
+            }
+
             //https://www.mercadopago.com.ar/developers/es/docs/checkout-pro/additional-content/your-integrations/notifications/webhooks#editor_2
             //https://github.com/mercadopago/sdk-java/blob/master/src/main/java/com/mercadopago/client/payment/PaymentClient.java
             //https://github.com/mercadopago/sdk-java/blob/fdb3724de22f63d6152671c7fa83449691c546a6/src/main/java/com/mercadopago/resources/payment/Payment.java
-            PaymentClient pc = new PaymentClient();
-
-            System.out.println(pc.get(64550388878l));
-
-
-
 
             return ResponseEntity.status(HttpStatus.OK).body("ok");
             //return ResponseEntity.status(HttpStatus.OK).body("{\"preferenceId\":\"" + "asdasdasd" + "\"}");
